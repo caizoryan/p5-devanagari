a2147f377a93e1d4cdbfc61fdb658af1f9b5bd14
Aaryan Pashine <pashineaaryan@gmail.com>
2023-03-23 23:36:10 -0400

    for bb

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let font;
 let masterPoints = [];
 let index = 0;
 let text = "Monकी";
-let disturbance = 10;
-let operator = "add";
+let disturbance = 10; // higher more anxiety
+let complexity = 0.05; //higher is more complexity
 // let text = "जुगाड़";
 function preload() {
   font = loadFont("chikki.otf");
@@ -11,42 +11,22 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 8; i++) {
-    masterPoints.push(
-      font.textToPoints(text, 100, 800, 600, {
-        sampleFactor: 0.001 + i * 0.003,
-      })
-    );
-  }
-  fill(255);
-  stroke(200, 70, 0);
-  setInterval(() => {
-    if (index === 7) index = 0;
-    index++;
-  }, 1000);
-  setInterval(() => {
-    if (disturbance === 40) operator = "subtract";
-    else if (disturbance === 0) operator = "add";
-    if (operator === "add") disturbance++;
-    else if (operator === "subtract") disturbance--;
-  }, 100);
+  masterPoints = font.textToPoints(text, 100, 800, 600, {
+    sampleFactor: complexity,
+  });
+  stroke(255);
 }
 
 function draw() {
-  background(0, 17);
-  for (let i = 0; i < masterPoints[index].length; i++) {
-    if (i + 2 > masterPoints[index].length) break;
+  background(0, 10);
+
+  for (let i = 0; i < masterPoints.length; i++) {
+    if (i + 2 > masterPoints.length) break;
     line(
-      masterPoints[index][i].x + random(disturbance),
-      masterPoints[index][i].y + random(disturbance),
-      masterPoints[index][i + 1].x + random(disturbance),
-      masterPoints[index][i + 1].y + random(disturbance)
+      masterPoints[i].x + random(disturbance),
+      masterPoints[i].y + random(disturbance),
+      masterPoints[i + 1].x + random(disturbance),
+      masterPoints[i + 1].y + random(disturbance)
     );
-    // rect(
-    //   points[i].x + random(10),
-    //   points[i].y + random(10),
-    //   random(10),
-    //   random(10)
-    // );
   }
 }
